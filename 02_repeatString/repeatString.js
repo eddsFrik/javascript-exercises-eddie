86188ededf23b7031a41d1f9b40c97d0e633628a
eddsFrik <129126536+eddsFrik@users.noreply.github.com>
2025-02-10 20:18:11 +0000

    test-passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    if(num === -1)  return "ERROR";
+    
+    let repeat = '';
+
+    while(num > 0) {
+
+        repeat += string;
+
+        num--;
+    }
+
+    return repeat;
 };
 
+
+repeatString('hey', 3);
 // Do not edit below this line
 module.exports = repeatString;
