637c597354ecac538998f6f39d7633c25365eced
eddsFrik <129126536+eddsFrik@users.noreply.github.com>
2025-02-10 21:15:36 +0000

    test-passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
+const reverseString = function(str) {
+
+    return str.split('').reverse().join('');
 
 };
 
+reverseString('Hello There');
 // Do not edit below this line
 module.exports = reverseString;
