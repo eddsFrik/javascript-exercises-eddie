1de4e1e5e95884d068767c38efca71149ced9608
eddsFrik <129126536+eddsFrik@users.noreply.github.com>
2025-02-13 23:26:57 +0000

    test-passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...num) {
+
+    return arr.filter(el => !num.includes(el));
+    
 };
 
+
+removeFromArray([1,2,3,4], 3);
 // Do not edit below this line
 module.exports = removeFromArray;
